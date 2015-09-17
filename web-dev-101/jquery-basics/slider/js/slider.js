d26e6e622f62d3c3203fd5ba6ba909f4b5897976
Alex Bradley <alexandre.bradley@alcatel-lucent.com>
2015-09-17 08:25:11 -0400

    finished slider. Re-wrote int/next to use a single variable.

--- web-dev-101/jquery-basics/slider/js/slider.js
+++ web-dev-101/jquery-basics/slider/js/slider.js
@@ -1,26 +1,74 @@
-sliderInt = 1;
-sliderNext = 2;
+var currentSliderPos = 1;
 
 $(document).ready(function() {
 	$("#slider > img#1").fadeIn(300);
 
 	startSlider();
 
+	// Putting this at the end of file means it is loaded before dom is ready?
+	// Works when placed in the on-load.
+	// http://stackoverflow.com/a/15347133/4580715
+	$("#slider > img").hover(
+		function() {
+			stopLoop();
+		},
+		function() {
+			startSlider();
+		}
+	);
+
+
 });
 
 function startSlider() {
 
 	count = $("#slider > img").size();
+	$("#sliderStatus").html("<p> Slider ACTIVE </p>");
 
 	loop = setInterval(function() {
 
+		currentSliderPos++;
+
+		if (currentSliderPos > count) {
+			currentSliderPos = 1;
+		}
+
 		$("#slider > img").fadeOut(300);
-		$("#slider > img#"+ sliderNext).fadeIn(300);
-		console.log("Current img = " + sliderNext);
+		$("#slider > img#"+ currentSliderPos).fadeIn(300);
+		console.log("Current img = " + currentSliderPos);
 
-		sliderInt = sliderNext;
-		sliderNext = sliderNext + 1;
+	}, 2000);
 
-	}, 3000);
+}
 
+function prev() {
+	showSlide(currentSliderPos - 1);
+}
+
+function next() {
+	showSlide(currentSliderPos + 1);
+}
+
+function stopLoop(){
+	window.clearInterval(loop);
+	$("#sliderStatus").html("<p> Slider DISABLED </p>");
+}
+
+function showSlide(id) {
+
+	stopLoop();
+
+	if (id > count) {
+		id = 1;
+	}
+	else if (id < 1) {
+		id = count;
+	}
+
+	$("#slider > img").fadeOut(300);
+	$("#slider > img#"+id).fadeIn(300);
+
+	currentSliderPos = id;
+
+	startSlider();
 }
