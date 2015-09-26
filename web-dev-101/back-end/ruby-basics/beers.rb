a61e0cdecf00d665e8618bd034c266eb1c4901ee
Alex Bradley <lopert@gmail.com>
2015-09-26 19:30:17 -0400

    ruby recursion.

---
+++ web-dev-101/back-end/ruby-basics/beers.rb
@@ -0,0 +1,14 @@
+# expected output
+#99 bottles of beer on the wall, 99 bottles of beer.
+#Take one down and pass it around, 98 bottles of beer on the wall.
+
+def beer_song number
+	puts number.to_s + " bottles of beer on the wall, " + number.to_s +  " bottles of beer."
+	number -= 1
+	puts "Take one down and pass it around, " + number.to_s +  " bottles of beer on the wall."
+	puts
+
+	beer_song(number) unless number == 0
+end
+
+beer_song(999)
