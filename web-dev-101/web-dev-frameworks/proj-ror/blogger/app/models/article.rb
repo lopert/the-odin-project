061b79979c2f40fa68df046f0f86538f715f1cd7
Alex Bradley <lopert@gmail.com>
2017-09-07 20:05:03 -0400

    jumpstartlab logger i0

--- web-dev-101/web-dev-frameworks/proj-ror/blogger/app/models/article.rb
+++ web-dev-101/web-dev-frameworks/proj-ror/blogger/app/models/article.rb
@@ -1,3 +1,2 @@
-class Article < ActiveRecord::Base
-  has_many :comments
+class Article < ApplicationRecord
 end
--- web-dev-101/web-dev-frameworks/proj-ror/blogger/db/schema.rb
+++ web-dev-101/web-dev-frameworks/proj-ror/blogger/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010174739) do
+ActiveRecord::Schema.define(version: 20170907233147) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,15 +18,5 @@ ActiveRecord::Schema.define(version: 20161010174739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "author_name"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
